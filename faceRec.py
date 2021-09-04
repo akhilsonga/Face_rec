99abb951492e109c2aabbc7c2933c32b2f3f653e
akhilsonga <81876214+akhilsonga@users.noreply.github.com>
2021-09-04 07:03:41 +0530

    Create faceRec.py

---
+++ faceRec.py
@@ -0,0 +1,180 @@
+import face_recognition as fr
+import os
+import cv2
+import face_recognition
+import numpy as np
+from cv2 import *
+from time import sleep
+
+
+def get_encoded_faces():
+    """
+    looks through the faces folder and encodes all
+    the faces
+
+    :return: dict of (name, image encoded)
+    """
+    encoded = {}
+
+    for dirpath, dnames, fnames in os.walk("./faces"):
+        for f in fnames:
+            if f.endswith(".jpg") or f.endswith(".png"):
+                face = fr.load_image_file("faces/" + f)
+                encoding = fr.face_encodings(face)[0]
+                encoded[f.split(".")[0]] = encoding
+
+    return encoded
+
+
+def unknown_image_encoded(img):
+    """
+    encode a face given the file name
+    """
+    face = fr.load_image_file("faces/" + img)
+    encoding = fr.face_encodings(face)[0]
+
+    return encoding
+
+
+def classify_face(im):
+    """
+    will find all of the faces in a given image and label
+    them if it knows what they are
+
+    :param im: str of file path
+    :return: list of face names
+    """
+    faces = get_encoded_faces()
+    faces_encoded = list(faces.values())
+    known_face_names = list(faces.keys())
+
+    img = cv2.imread(im, 1)
+    #img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+    #img = img[:,:,::-1]
+ 
+    face_locations = face_recognition.face_locations(img)
+    unknown_face_encodings = face_recognition.face_encodings(img, face_locations)
+
+    face_names = []
+    for face_encoding in unknown_face_encodings:
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(faces_encoded, face_encoding)
+        name = "Unknown"
+
+        # use the known face with the smallest distance to the new face
+        face_distances = face_recognition.face_distance(faces_encoded, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+
+        face_names.append(name)
+
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Draw a box around the face
+            cv2.rectangle(img, (left-20, top-20), (right+20, bottom+20), (255, 0, 0), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(img, (left-20, bottom -15), (right+20, bottom+20), (255, 0, 0), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(img, name, (left -20, bottom + 15), font, 1.0, (255, 255, 255), 2)
+
+
+    # Display the resulting image
+    while True:
+
+        cv2.imshow('Video', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            return face_names 
+#"""
+"""
+cap = cv2.VideoCapture(0)
+
+cam = VideoCapture(0)   # 0 -> index of camera
+s, img = cam.read()
+if s:    # frame captured without any errors
+    namedWindow("cam-test",CV_WINDOW_AUTOSIZE)
+    imshow("cam-test",img)
+    waitKey(0)
+    destroyWindow("cam-test")
+    imwrite("filename.jpg",img)
+
+
+deoCaptureObject = cv2.VideoCapture(0)
+result = True
+while(result):
+    ret,frame = videoCaptureObject.read()
+    cv2.imwrite("NewPicture.jpg",frame)
+    result = False
+videoCaptureObject.release()
+cv2.destroyAllWindows()
+.........................
+# import the opencv library 
+import cv2 
+
+
+# define a video capture object 
+vid = cv2.VideoCapture(0) 
+
+while(True): 
+	
+	# Capture the video frame 
+	# by frame 
+	ret, frame = vid.read() 
+
+	# Display the resulting frame 
+	cv2.imshow('frame', frame) 
+	
+	# the 'q' button is set as the 
+	# quitting button you may use any 
+	# desired button of your choice 
+	if cv2.waitKey(1) & 0xFF == ord('q'): 
+		break
+
+# After the loop release the cap object 
+vid.release() 
+# Destroy all the windows 
+cv2.destroyAllWindows() 
+
+
+
+
+
+
+#"""
+"""
+
+# define a video capture object 
+vid = cv2.VideoCapture(0) 
+
+while(True): 
+	
+	# Capture the video frame 
+	# by frame 
+	ret, frame = vid.read() 
+
+	# Display the resulting frame 
+	cv2.imshow('frame', frame) 
+	
+	# the 'q' button is set as the 
+	# quitting button you may use any 
+	# desired button of your choice 
+	if cv2.waitKey(1) & 0xFF == ord('q'): 
+		break
+
+# After the loop release the cap object 
+vid.release() 
+# Destroy all the windows 
+cv2.destroyAllWindows() 
+
+""
+"""
+cap = cv2.VideoCapture(0)
+ret, img = cap.read()
+cv2.imwrite("test.jpg")
+#cv2.imshow('img',img)
+cv2.waitKey(1);
+cap.release()
+cv2.destroyAllWindows()
+print(classify_face("test.jpg"))
+
+
